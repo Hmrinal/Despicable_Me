edea496cbbfd5e5ca04fe1ec47661914e284421e
Hmrinal <mrinaltyagi1912@gmail.com>
2020-12-28 22:15:07 +0530

    basic changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
      console.log("clicked");
  })
 */
-
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
